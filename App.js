85103f108761d5c2957afa86443869e4da2a034d
Carlos <carlos012100@gmail.com>
2025-09-29 07:59:49 +0200

    [newcommit]

---
+++ App.js
@@ -0,0 +1,60 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import { ScrollView, StatusBar } from 'react-native';
+import Header from './components/Header';
+import Home from './screens/Home';
+import Friends from './screens/Friends';
+import Groups from './screens/Groups';
+import Profile from './screens/Profile';
+import Notifications from './screens/Notification';
+import Menu from './screens/Menu';
+import { NavigationContainer } from '@react-navigation/native';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+
+const Tab = createMaterialTopTabNavigator();
+
+const App = () => {
+  return (
+    <>
+      <StatusBar backgroundColor="#FFFFFF" barStyle="dark-content" />
+      
+      {/* ScrollView is only for the header */}
+      <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+        <Header />
+      </ScrollView>
+
+      <NavigationContainer>
+        <Tab.Navigator
+          screenOptions={({ route }) => ({
+            tabBarShowIcon: true, // updated from showIcon
+            tabBarLabel: () => null, // hides labels
+            tabBarActiveTintColor: '#3a86e9',
+            tabBarInactiveTintColor: '#9F9F9F',
+            tabBarIcon: ({ color }) => {
+              let iconName;
+
+              if (route.name === 'Home') iconName = 'home';
+              else if (route.name === 'Friends') iconName = 'account-multiple-outline';
+              else if (route.name === 'Groups') iconName = 'account-group';
+              else if (route.name === 'Profile') iconName = 'account-circle-outline';
+              else if (route.name === 'Notifications') iconName = 'bell-outline';
+              else if (route.name === 'Menu') iconName = 'menu';
+
+              return <MaterialCommunityIcons name={iconName} size={26} color={color} />;
+            },
+          })}
+        >
+          <Tab.Screen name="Home" component={Home} />
+          <Tab.Screen name="Friends" component={Friends} />
+          <Tab.Screen name="Groups" component={Groups} />
+          <Tab.Screen name="Profile" component={Profile} />
+          <Tab.Screen name="Notifications" component={Notifications} />
+          <Tab.Screen name="Menu" component={Menu} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </>
+  );
+};
+
+export default App;
---
+++ components/Toolbar.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import styled from 'styled-components/native'
+import {MaterialCommunityIcons} from '@expo/vector-icons'
+import Avatar from './Avatar'
+
+const Container = styled.View`
+    width: 100%;
+    HeaderHeightContext: 92px`
+
+const Row = styled.View `
+    flex-direction: row;
+    background: #FFFFFF;
+    width: 100%;
+    padding: 0 11px;
+    align-items: center;
+    `
+const Input = styled.TextInput`
+    height: 40px;
+    width: 85%;
+    padding: 0 8px;
+    border: 1px solid #DDD;
+    margin: 10px;
+    border-radius: 30px;
+    `
+const Divider = styled.View`
+    width: 100%;
+    height: 0.5px;
+    background:  #DDDDDD;
+    `
+
+const Separator = styled.View`
+    width: 1px;
+    height: 26px;
+    background: #F0F0F0;`
+
+const ToolBar = () => {
+    return (
+        <Container>
+            <Row>
+                <Avatar source={require('../assets/12.jpg')} />
+                <Input placeholder = "¿Que esta pensando?" />
+            </Row>
+            <Divider/>
+            <Row>
+                <Menu >
+                    <MaterialCommunityIcons name ="video-account" size={20} color="#F44337"/>
+                </Menu>
+                <Separator/>
+                <Menu>
+                    <MaterialCommunityIcons name= "video-plus" size={20} colors="#A748EE"/>
+                    <MenuText>Sala</MenuText>
+                </Menu>
+            </Row>
+        </Container>
+    )
+}
+export default ToolBar
